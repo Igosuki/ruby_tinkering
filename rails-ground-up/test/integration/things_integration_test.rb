f2afc7865683b6d70790f54a511cbcdbb366ecd4
Guillaume Balaine <igosuki@gmail.com>
2013-06-22 08:55:02 +0200

    Small refacto of this test plus relevant assertions

--- rails-ground-up/test/integration/things_integration_test.rb
+++ rails-ground-up/test/integration/things_integration_test.rb
@@ -3,16 +3,17 @@ require 'test_helper'
 class ThingFlowsTest < ActionDispatch::IntegrationTest
   fixtures :all
   setup do 
-
+    @id = 3
   end
   # Replace this with your real tests.
   test "complete scenario" do
     post_via_redirect "/things", :thing => {:name => "Alex", :description => "Intg test"}
-    assert_equal "/things/3", path
-    put_via_redirect(thing_path({:id => 3, :name => "A new name"}))
-    assert_equal "/things/3", path
-    delete(thing_path(3))
-    get(thing_path(3))
+    assert_equal thing_path(@id), path
+    put_via_redirect(thing_path({:id => @id, :name => "A new name"}))
+    assert_equal thing_path(@id), path
+    delete(thing_path(@id))
+    assert redirect?
+    get(thing_path(@id))
     assert_response 404
   end
 end
